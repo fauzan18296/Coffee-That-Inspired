6029e81a3e217b8fb61292df972aaa00e5be4ef4
Ahmad Fauzan <fauzan18296@gmail.com>
2025-01-11 22:28:44 +0700

    style(navigation): add background color of navigation

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -4,11 +4,11 @@ import Navbar from './Navbar'
 import Button from '../Ui/Button'
 const Header = () => {
   return (
-    <header className="flex dark:bg-black/55 dark:backdrop-blur-md bg-white p-3 w-screen justify-between items-center shadow-xl fixed">
+    <header className="flex bg-white p-3 w-screen justify-between items-center shadow-xl fixed">
       <Navbar>
          <a className='flex gap-2 items-center' href="/">
           <img src={ Logo } alt="Logo" className="w-12 rounded-md object-cover" />
-          <h1 className="text-primary text-2xl font-bold"><span className="dark:text-white">Coffee</span> <span className='dark:text-primary'>_Inspires</span></h1>
+          <h1 className="text-primary text-2xl font-bold"><span>Coffee</span> <span className='dark:text-primary'>_Inspires</span></h1>
         </a>
       </Navbar>
       <Button classname='md:hidden mx-5 text-primary' type='button' name={<Menu size={20} />} />
