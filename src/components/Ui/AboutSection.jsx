8676afd5e3939d86bc06cda66c20316711c4f3c0
abiyanssa <abiyansamawi.alk@gmail.com>
2025-01-15 06:58:15 +0700

    fix(background): fix bg color in about page section about products

--- src/components/Ui/AboutSection.jsx
+++ src/components/Ui/AboutSection.jsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
         { coffeeProducts.map((product) => {
           return (
             <div key={product.id}>
-              <div className={`flex bg-white rounded-lg shadow-lg size-full justify-center items-center flex-col ${darkMode && 'bg-primary text-white'}`}>
+              <div className={`flex  rounded-lg shadow-lg size-full justify-center items-center flex-col ${darkMode && 'bg-primary text-white' || 'bg-white'}`}>
                 <Image classname="  max-mobile:mx-auto" image={product.image_url} alt="Coffee Image" />
                 <h1 className={`text-2xl max-mobile:text-xl font-bold text-primary tracking-wide ${darkMode && 'text-white'}`}>{product.name}</h1>
                 <p className='text-xl mx-4 max-mobile:text-base tracking-wide'>{product.description}</p>
