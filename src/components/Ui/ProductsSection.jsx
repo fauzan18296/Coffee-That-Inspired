6efd682968f8f7e0d25187e220c920a3f900e991
Ahmad Fauzan <fauzan18296@gmail.com>
2025-01-29 12:38:16 +0700

    UI(products): Change font size name of products

--- src/components/Ui/ProductsSection.jsx
+++ src/components/Ui/ProductsSection.jsx
@@ -11,15 +11,15 @@ const ProductsSection = () => {
           <h1 className="text-2xl max-mobile:text-lg mt-32 text-primary font-bold tracking-wide ">Products ☕</h1>
       <div className="container flex items-center justify-center flex-col my-7">
         <div className='flex justify-center items-center'>
-          <div className='grid grid-cols-3 gap-8 max-mobile:grid-cols-2 max-mobile:mx-6'>
+          <div className='grid grid-cols-3 gap-8 max-mobile:grid-cols-1 max-mobile:mx-6'>
           {coffeeProducts.map((product) => {
             return (
               <div key={product.id}>
                 <div className={`${darkMode && "bg-primary text-white" || "bg-white"} rounded-md shadow-lg size-full`}>
                   <div className='text-center tracking-wide'>
                   <Image classname="size-full" image={product.image_url} alt="Image Coffee" />
-                    <h2 className={`text-center text-xl max-mobile:text-base text-primary font-bold ${darkMode && "text-white" || "text-primary"}`}>{product.name}</h2>
-                    <p className='text-lg font-medium max-mobile:text-base'>{new Intl.NumberFormat("en-us", { style:"currency", currency:"USD", maximumFractionDigits: 0}).format(product.price)}</p>
+                    <h2 className={`text-center text-xl max-mobile:text-xl text-primary font-bold ${darkMode && "text-white" || "text-primary"}`}>{product.name}</h2>
+                    <p className='text-lg font-semibold max-mobile:text-xl'>{new Intl.NumberFormat("en-us", { style:"currency", currency:"USD", maximumFractionDigits: 0}).format(product.price)}</p>
                     </div>
                   </div>
               </div>
