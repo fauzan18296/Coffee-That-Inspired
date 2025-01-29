15b12f66d4decfc8f20dcb9dfc5807f75017cf1a
Ahmad Fauzan <fauzan18296@gmail.com>
2025-01-29 14:25:06 +0700

    Ui(products): Change font size responsive name of products

--- src/components/Ui/ProductsSection.jsx
+++ src/components/Ui/ProductsSection.jsx
@@ -1,16 +1,24 @@
 import { useCoffeeProducts } from '../../hooks/useCoffeeProducts'
 import Image from './Image'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { DarkModeContext } from '../../context/DarkMode'
 
 const ProductsSection = () => {
   const { coffeeProducts } = useCoffeeProducts()
   const { darkMode } = useContext(DarkModeContext)
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1
+    }
+  ])
+
   return (
     <div className='flex justify-center  items-center flex-col min-h-screen w-full'>
           <h1 className="text-2xl max-mobile:text-lg mt-32 text-primary font-bold tracking-wide ">Products ☕</h1>
       <div className="container flex items-center justify-center flex-col my-7">
         <div className='flex justify-center items-center'>
+          <div className='flex justify-center items-center text-xl'>
           <div className='grid grid-cols-3 gap-8 max-mobile:grid-cols-1 max-mobile:mx-6'>
           {coffeeProducts.map((product) => {
             return (
@@ -21,14 +29,15 @@ const ProductsSection = () => {
                     <h2 className={`text-center text-xl max-mobile:text-xl text-primary font-bold ${darkMode && "text-white" || "text-primary"}`}>{product.name}</h2>
                     <p className='text-lg font-semibold max-mobile:text-xl'>{new Intl.NumberFormat("en-us", { style:"currency", currency:"USD", maximumFractionDigits: 0}).format(product.price)}</p>
                     </div>
-                  </div>
+                </div>
               </div>
             )
           })}
             </div>
         </div>
         </div>
-    </div>
+      </div>
+      </div>
   )
 }
 
