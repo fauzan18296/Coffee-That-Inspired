a5a3fa13b22c1a931c980214c60443dacf285503
Ahmad Fauzan <fauzan18296@gmail.com>
2025-04-10 16:16:30 +0700

    Revert "add(localStorange): +add localStorage in CartProduct"
    This reverts commit f1a3b905d3dede64c53305cd236a69e680082e39.

--- src/components/Ui/CartProducts.jsx
+++ src/components/Ui/CartProducts.jsx
@@ -9,7 +9,6 @@ const CartProducts = (() => {
   const { coffeeIceProducts } = useCoffeeProducts()
 
   useEffect(() => {
-    JSON.parse(localStorage.getItem('cart'))
     localStorage.setItem('cart', JSON.stringify(cart))
     return () => {
       localStorage.removeItem('cart')
