948fa1575e0b19ff48bb909e7933801c1beb8c98
Ahmad Fauzan <fauzan18296@gmail.com>
2025-04-24 14:21:37 +0700

    UI(SectionPage): Change flex basis in image items about page and home page

--- src/components/Ui/HeroSection.jsx
+++ src/components/Ui/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <h1 className='text-3xl max-tablet:text-xl font-bold tracking-wider font-Poppins'>Welcome To <span className='text-primary'>Coffee Inspired,</span></h1>
         <p className='tracking-wide text-2xl max-tablet:text-base'>Discover the perfect blend of aroma and taste in every sip. Let our coffee awaken your senses and spark your inspiration, cup after cup.</p>
           </div>
-      <Image classname='mx-7 md:size-2/6 max-mobile:size-8/12 basis-full' 
+      <Image classname='mx-7 size-2/6 max-mobile:size-8/12 basis-1/2' 
         image={HeroImg} alt="coffee"></Image>
         </div>
   )
